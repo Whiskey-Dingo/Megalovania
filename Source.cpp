bc8c17695d334ef605243aa6b261640e59533113
Whiskey-Dingo <75032236+Whiskey-Dingo@users.noreply.github.com>
2020-11-25 19:50:44 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+    system("pause");
+    Beep(293,150);
+    Beep(293,150);
+    Beep(587,350);
+    Beep(440,375);
+    Beep(0, 100);
+    Beep(416,375);
+    Beep(392,325);
+    Beep(349,350);
+    Beep(293,150);
+    Beep(349,150);
+    Beep(392,150);
+    
+    Beep(261,150);
+    Beep(261,150);
+    Beep(587,350);
+    Beep(440,375);
+    Beep(0, 100);
+    Beep(416,375);
+    Beep(392,325);
+    Beep(349,350);
+    Beep(293,150);
+    Beep(349,150);
+    Beep(392,150);
+
+    
+    Beep(246,150);
+    Beep(246,150);
+    Beep(587,350);
+    Beep(440,375);
+    Beep(0, 100);
+    Beep(416,375);   
+    Beep(392,375);
+    Beep(349,325);
+    Beep(293,150);
+    Beep(349,150);
+    Beep(392,150);
+
+    Beep(220, 150);
+    Beep(220, 150);
+    Beep(587, 350);
+    Beep(440, 375);
+    Beep(0, 100);
+    Beep(416, 375);
+    Beep(392, 375);
+    Beep(349, 325);
+    Beep(293, 150);
+    Beep(349, 150);
+    Beep(392, 150);
+
+    Beep(293, 150);
+    Beep(293, 150);
+    Beep(587, 350);
+    Beep(440, 375);
+    Beep(0, 100);
+    Beep(416, 375);
+    Beep(392, 325);
+    Beep(349, 350);
+    Beep(293, 150);
+    Beep(349, 150);
+    Beep(392, 150);
+
+    Beep(261, 150);
+    Beep(261, 150);
+    Beep(587, 350);
+    Beep(440, 375);
+    Beep(0, 100);
+    Beep(416, 375);
+    Beep(392, 325);
+    Beep(349, 350);
+    Beep(293, 150);
+    Beep(349, 150);
+    Beep(392, 150);
+
+
+    Beep(246, 150);
+    Beep(246, 150);
+    Beep(587, 350);
+    Beep(440, 375);
+    Beep(0, 100);
+    Beep(416, 375);
+    Beep(392, 375);
+    Beep(349, 325);
+    Beep(293, 150);
+    Beep(349, 150);
+    Beep(392, 150);
+
+    Beep(220, 150);
+    Beep(220, 150);
+    Beep(587, 350);
+    Beep(440, 375);
+    Beep(0, 100);
+    Beep(416, 375);
+    Beep(392, 375);
+    Beep(349, 325);
+    Beep(293, 150);
+    Beep(349, 150);
+    Beep(392, 150);
+
+
+    return 0;
+}
